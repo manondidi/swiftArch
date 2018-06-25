2e3d5def6accbcc0de41ffcd44990139e38d76a5
czq <790512128@qq.com>
2018-06-25 10:04:30 +0800

    SateCollectionView封装

---
+++ swiftArch/common/component/collectionView/UICollectionView+Binder.swift
@@ -0,0 +1,19 @@
+//
+//  UICollectionView+Binder.swift
+//  swiftArch
+//
+//  Created by czq on 2018/6/25.
+//  Copyright © 2018年 czq. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionView{
+
+    func registerCellClass(cellClass:AnyClass,modelClass:AnyClass) {
+        self.register(cellClass, forCellReuseIdentifier:String(describing:modelClass.self) )
+    }
+    func registerCellNib(nib:UINib,modelClass:AnyClass){
+        self.register(nib, forCellReuseIdentifier: String(describing:modelClass.self))
+    }
+}
