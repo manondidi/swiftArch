35b27c941f8ab24296ed4eb371148452fd6bdf19
码农弟弟 <790512128@qq.com>
2019-04-03 10:06:34 +0800

    代码格式化

--- Example/swiftArch/ViewController.swift
+++ Example/swiftArch/ViewController.swift
@@ -6,19 +6,16 @@
 //  Copyright (c) 2018 czq. All rights reserved.
 //
 
-import UIKit
 import swiftArch
+import UIKit
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor=UIColor.white
-        
-        
-        
-        let btn3=UIButton(type: UIButton.ButtonType.roundedRect)
-        self.view.addSubview(btn3)
-        btn3.snp.makeConstraints { (make) in
+        view.backgroundColor = UIColor.white
+
+        let btn3 = UIButton(type: UIButton.ButtonType.roundedRect)
+        view.addSubview(btn3)
+        btn3.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalToSuperview().offset(140)
         }
@@ -26,77 +23,70 @@ class ViewController: UIViewController {
         btn3.onTap {
             self.navigationController?.pushViewController(DemoViewController(), animated: true)
         }
-        
-        
-        let btn4=UIButton(type: UIButton.ButtonType.roundedRect)
-        self.view.addSubview(btn4)
-        btn4.snp.makeConstraints { (make) in
+
+        let btn4 = UIButton(type: UIButton.ButtonType.roundedRect)
+        view.addSubview(btn4)
+        btn4.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(btn3.snp.bottom).offset(30)
         }
-        btn4.titleLabel?.numberOfLines=0
+        btn4.titleLabel?.numberOfLines = 0
         btn4.setTitle("PaingTalbeDemoViewController gameList,section方案 pagenum分页策略", for: UIControl.State.normal)
         btn4.onTap {
             [weak self] in
             self?.navigationController?.pushViewController(PaingTalbeDemoViewController(), animated: true)
         }
-        
-        
-        let btn5=UIButton(type: UIButton.ButtonType.roundedRect)
-        self.view.addSubview(btn5)
-        btn5.snp.makeConstraints { (make) in
+
+        let btn5 = UIButton(type: UIButton.ButtonType.roundedRect)
+        view.addSubview(btn5)
+        btn5.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(btn4.snp.bottom).offset(30)
         }
-        btn5.titleLabel?.numberOfLines=0
+        btn5.titleLabel?.numberOfLines = 0
         btn5.setTitle("PagingOffsetIdDemoViewController banner offset分页策略", for: UIControl.State.normal)
         btn5.onTap {
             [weak self] in
             self?.navigationController?.pushViewController(PagingOffsetIdDemoViewController(), animated: true)
         }
-        
-        
-        let btn6=UIButton(type: UIButton.ButtonType.roundedRect)
-        self.view.addSubview(btn6)
-        btn6.snp.makeConstraints { (make) in
+
+        let btn6 = UIButton(type: UIButton.ButtonType.roundedRect)
+        view.addSubview(btn6)
+        btn6.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(btn5.snp.bottom).offset(30)
         }
-        btn6.titleLabel?.numberOfLines=0
+        btn6.titleLabel?.numberOfLines = 0
         btn6.setTitle("FeedsDemoViewController手动计算行高 feed流", for: UIControl.State.normal)
         btn6.onTap {
             [weak self] in
             self?.navigationController?.pushViewController(FeedsDemoViewController(), animated: true)
         }
-        
-        
-        let btn7=UIButton(type: UIButton.ButtonType.roundedRect)
-        self.view.addSubview(btn7)
-        btn7.snp.makeConstraints { (make) in
+
+        let btn7 = UIButton(type: UIButton.ButtonType.roundedRect)
+        view.addSubview(btn7)
+        btn7.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(btn6.snp.bottom).offset(30)
         }
-        btn7.titleLabel?.numberOfLines=0
+        btn7.titleLabel?.numberOfLines = 0
         btn7.setTitle("PagingCollectionDemoViewController collectionView pagenum分页策略", for: UIControl.State.normal)
         btn7.onTap {
             [weak self] in
             self?.navigationController?.pushViewController(PagingCollectionDemoViewController(), animated: true)
         }
-        
-        let btn8=UIButton(type: UIButton.ButtonType.roundedRect)
-        self.view.addSubview(btn8)
-        btn8.snp.makeConstraints { (make) in
+
+        let btn8 = UIButton(type: UIButton.ButtonType.roundedRect)
+        view.addSubview(btn8)
+        btn8.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(btn7.snp.bottom).offset(30)
         }
-        btn8.titleLabel?.numberOfLines=0
+        btn8.titleLabel?.numberOfLines = 0
         btn8.setTitle("测试新建的模板", for: UIControl.State.normal)
         btn8.onTap {
             [weak self] in
             self?.navigationController?.pushViewController(TempTestViewController(), animated: true)
         }
     }
-
-
 }
-
